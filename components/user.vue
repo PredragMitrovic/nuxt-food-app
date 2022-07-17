60de07745ccf7b0c66f8ea8296cf4da68514bbfd
predrag <predrag.mitrovic@sunzinet.com>
2022-07-17 19:04:15 +0200

    Develop method for filter and logic for display layouts

---
+++ components/user.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+      <h1> {{ user.name }}</h1>
+      <p> {{ user.username }}</p>
+      <p> {{ user.id }}</p>
+      <a href="#"  @click="findId">Delete </a>
+      <nuxt-link :to="`/user/${user.id}`">Edit  user</nuxt-link>
+    </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+
+export default {
+    props: ['user'],
+    methods: {
+    findId(){
+      this.DELETE_USER(this.user.id)
+    },
+        ...mapActions([
+            'DELETE_USER'
+        ])
+    },
+};
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
